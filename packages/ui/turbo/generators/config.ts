61d5b6d933aef155363cb3dbabc7ceba52a481fd
Juan Ignacio De los Santos <juanignaciodelossantos@gmail.com>
2025-04-22 14:58:01 -0300

    feat: create generated component template

--- packages/ui/turbo/generators/config.ts
+++ packages/ui/turbo/generators/config.ts
@@ -1,29 +1,36 @@
 import type { PlopTypes } from "@turbo/gen";
 
-// Learn more about Turborepo Generators at https://turbo.build/docs/core-concepts/monorepos/code-generation
-
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
-  // A simple generator to add a new React component to the internal UI library
   plop.setGenerator("react-component", {
     description: "Adds a new react component",
     prompts: [
       {
         type: "input",
         name: "name",
-        message: "What is the name of the component?",
+        message: "Name of the component - Should not contain info about the domain (eg: Card ✅ - UserCard ❌",
+        validate: value => !!value || "Name is mandatory"
       },
     ],
     actions: [
       {
         type: "add",
-        path: "src/{{kebabCase name}}.tsx",
+        path: "src/{{kebabCase name}}/index.ts",
+        templateFile: "templates/index.hbs",
+      },
+      {
+        type: "add",
+        path: "src/{{kebabCase name}}/{{kebabCase name}}.tsx",
         templateFile: "templates/component.hbs",
       },
       {
-        type: "append",
-        path: "package.json",
-        pattern: /"exports": {(?<insertion>)/g,
-        template: '    "./{{kebabCase name}}": "./src/{{kebabCase name}}.tsx",',
+        type: "add",
+        path: "src/{{kebabCase name}}/{{kebabCase name}}.styles.ts",
+        templateFile: "templates/styles.hbs",
+      },
+      {
+        type: "add",
+        path: "src/{{kebabCase name}}/{{kebabCase name}}.test.ts",
+        templateFile: "templates/test.hbs",
       },
     ],
   });
---
+++ packages/ui/turbo/generators/templates/styles.hbs
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 14,
+  }
+});
